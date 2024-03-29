bc389a064c2eaa079c4196a0cd04d476896fc7e3
СвятоСвят <qureqr@gmail.com>
2024-03-30 01:52:35 +0200

    test

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const mineflayer = require('mineflayer')
+const bot = mineflayer.createBot({
+    host: 'localhost',
+    port: 25565,
+    username: 'Player',
+    password: 'password'
+})
+
+
+const options = {
+    host: 'localhost', // Change this to the ip you want.
+    port: 25565 // Change this to the port you want.
+}
+
+
 const a = 5
 console.log(a)
