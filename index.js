00b6f0d3bb7b9f7c6490ca5c62a3b5aa5f7ecce1
СвятоСвят <qureqr@gmail.com>
2024-04-01 12:37:51 +0300

    бот теперь умеет делать слюнявый(выглядит круто)

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ bot.once('spawn', () => {
     const mcData = require('minecraft-data')(bot.version);
     const defaultMove = new Movements(bot, mcData);
     bot.pathfinder.setMovements(defaultMove);
+
 });
 
 bot.on('chat', (username, message) => {
@@ -33,26 +34,31 @@ bot.on('death', (username, message) => {
     bot.chat(randomWord);
 })
 bot.on('chat', (username, message) => {
-
     if(message === 'сделай мне слюнявый'){
         bot.chat('ща спылисосим')
-        intervalId = setInterval(() =>{
-        let word = ['фпфпфпфп', 'горк', 'слёрп'];
-        let randomWords = word[Math.floor(Math.random() * word.length)];
-        bot.chat(randomWords)
-        }, 1000 )
         let target = bot.players[username]?.entity;
         const p = target.position;
         bot.pathfinder.setGoal(new GoalNear(p.x, p.y, p.z, 1));
 
-
-
+        // Проверяем, достиг ли бот своей цели
+        const checkIfReachedGoal = setInterval(() => {
+            if (!bot.pathfinder.isMoving()) {
+                const distance = bot.entity.position.distanceTo(p);
+                if (distance <= 2) {
+                    bot.setControlState('sneak', true);
+                    intervalId = setInterval(() =>{
+                        let word = ['фпфпфпфп', 'горк', 'слёрп'];
+                        let randomWords = word[Math.floor(Math.random() * word.length)];
+                        bot.chat(randomWords)
+                    }, 1000)
+                    clearInterval(checkIfReachedGoal);
+                }
+            }
+        }, 1000);
     }
+
     if(message === 'харе'){
         bot.chat('ладно(((')
         clearInterval(intervalId);
     }
-
-
-
-})
+});
