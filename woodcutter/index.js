827d93c5f9264f0243cfacdfc454b843822d2dde
СвятоСвят <qureqr@gmail.com>
2024-04-07 20:37:34 +0300

    создан отдельный бот для добычи дерева

---
+++ woodcutter/index.js
@@ -0,0 +1,37 @@
+const mineflayer = require('mineflayer');
+const collectBlock = require('mineflayer-collectblock').plugin;
+const minecraftData = require('minecraft-data');
+
+const bot = mineflayer.createBot({
+    host: 'localhost', // сервер Minecraft
+    username: 'инокентий_лесоруб', // имя бота
+    port:55111
+});
+bot.loadPlugin(require('mineflayer-collectblock').plugin)
+async function collectoak() {
+    const oakLogId = mcData.blocksByName.oak_log.id;
+    const block = bot.findBlock({
+        matching: oakLogId,
+        maxDistance: 64
+    })
+
+    if (block && block.type === oakLogId) {
+        try {
+            await bot.collectBlock.collect(block)
+            collectoak()
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
+
+bot.once('spawn', () => {
+    mcData = require('minecraft-data')(bot.version)
+})
+
+
+bot.on("chat", (username, message) => {
+  if(message === 'добудь мне дерева'){
+      collectoak()
+  }
+})
